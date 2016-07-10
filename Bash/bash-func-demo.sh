c820ac4437422d366afd3af18b7f52416c334053
Nischay Rathor <nishchayrathore@gmail.com>
2016-07-10 21:57:59 +0530

    modfied bash-func-demo.sh

--- Bash/bash-func-demo.sh
+++ Bash/bash-func-demo.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 function_hello() {
   echo "I am a demo for functions in shells"
-  exit 99 #message delivered 
+#  exit 99 #message delivered 
 }
 
 function_hello
+
+today() {
+    echo -n "Today's date is: "
+    date +"%A, %B %-d, %Y"
+}
+today
